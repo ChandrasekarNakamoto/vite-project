c98ab9f25d2bb65191e4916cb90b25263078e726
ChandrasekarNakamoto <chandrasekar2018105518@gmail.com>
2022-10-30 19:13:48 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import {FaGithub} from 'react-icons/fa'
+import {FaGithub,FaTwitter,FaFacebook, FaInstagram} from 'react-icons/fa'
 
 
 export default function Footerpart(){
     return(
         <div>
             <footer>
-                <a href="https://twitter.com/home" target='_blank'><button><i className='fab fa-twitter'></i></button></a>
-                <a href="https://www.facebook.com/" target='_blank'><button><i className="fa fa-facebook-square"></i></button></a>
-                <a href="https://www.instagram.com/" target='_blank'><button> <i className='fab fa-instagram'></i></button></a>
-                <a href="https://github.com/ChandrasekarNakamoto" target='_blank'><button ><i className="fa fa-github"></i></button></a>
+                <a href="https://twitter.com/home" target='_blank'><button><i><FaTwitter /></i></button></a>
+                <a href="https://www.facebook.com/" target='_blank'><button><i><FaFacebook /></i></button></a>
+                <a href="https://www.instagram.com/" target='_blank'><button> <i><FaInstagram /></i></button></a>
                 <a href="https://github.com/ChandrasekarNakamoto" target='_blank'><button ><i><FaGithub /></i></button></a>
             </footer>
         </div>
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import image from '../assets/Chandrasekar_image.jpg'
+import {FaLinkedin,FaMail} from 'react-icons/fa'
+
 
 export default function Info(){
     return(
@@ -9,8 +11,8 @@ export default function Info(){
             <h4>Frontend Developer</h4>
             <p>chandrasekar.website</p>
         <div>
-        <a href="mailto: [chandrasekarceg@gmail.com]?subject= &body=" target='_blank'><button><i className='fas fa-envelope'></i> Email</button></a>
-        <a href="https://www.linkedin.com/in/chandrasekar-r-ba7366224/" target='_blank'><button> <i className='fab fa-linkedin-in'></i>LinkedIn</button></a>
+        <a href="mailto: [chandrasekarceg@gmail.com]?subject= &body=" target='_blank'><button><i><FaMail /></i> Email</button></a>
+        <a href="https://www.linkedin.com/in/chandrasekar-r-ba7366224/" target='_blank'><button> <i><FaLinkedin /></i>LinkedIn</button></a>
 
         </div>
         </main>
