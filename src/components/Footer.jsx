13a71b4bc77168313943fe45228a02fc6f7198db
ChandrasekarNakamoto <chandrasekar2018105518@gmail.com>
2022-10-30 19:06:21 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaGithub} from 'react-icons/fa'
 
 
 export default function Footerpart(){
@@ -9,6 +10,7 @@ export default function Footerpart(){
                 <a href="https://www.facebook.com/" target='_blank'><button><i className="fa fa-facebook-square"></i></button></a>
                 <a href="https://www.instagram.com/" target='_blank'><button> <i className='fab fa-instagram'></i></button></a>
                 <a href="https://github.com/ChandrasekarNakamoto" target='_blank'><button ><i className="fa fa-github"></i></button></a>
+                <a href="https://github.com/ChandrasekarNakamoto" target='_blank'><button ><i><FaGithub /></i></button></a>
             </footer>
         </div>
     )
