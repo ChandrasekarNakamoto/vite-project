f0665b15946df8726ef6655683ed2ff385eb01dc
ChandrasekarNakamoto <chandrasekar2018105518@gmail.com>
2022-10-31 21:14:14 +0530

    commit

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -5,7 +5,7 @@ export default function Interests(){
     return(
         <div className='interests'>
             <h2>Interests</h2>
-            <p>Food expert. Defi degen. Reader. Internet fanatic. Entrepreneur. Travel geek. Coder. Jpeg dealer.Crypto geek</p>
+            <p>Food expert. Defi degen. Reader. Internet fanatic. Entrepreneur. Travel geek. Coder. Jpeg dealer. Crypto geek.</p>
         </div>
     )
 }
